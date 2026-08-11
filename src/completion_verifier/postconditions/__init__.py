91b8edae519b44b5801d55fcf79f22eec71e576a
Luca Panayiotou <lucapanay@icloud.com>
2026-08-11 20:03:11 +0100

    feat: expose postcondition contract models

---
+++ src/completion_verifier/postconditions/__init__.py
@@ -0,0 +1,17 @@
+from .models import (
+    DirectoryContract,
+    JsonObjectContract,
+    PostconditionContract,
+    PostconditionObservation,
+    TextFileContract,
+    validate_relative_path,
+)
+
+__all__ = [
+    "DirectoryContract",
+    "JsonObjectContract",
+    "PostconditionContract",
+    "PostconditionObservation",
+    "TextFileContract",
+    "validate_relative_path",
+]
